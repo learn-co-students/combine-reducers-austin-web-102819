83e0f2bb76f9cdc9b65f47ce391037134c5fe238
fmwong77 <fongmeiw@gmail.com>
2020-01-27 21:34:12 -0600

    Done.

--- src/components/AuthorInput.js
+++ src/components/AuthorInput.js
@@ -4,42 +4,47 @@ import uuid from 'uuid';
 import { connect } from 'react-redux';
 
 export class AuthorInput extends Component {
+	state = {
+		authorName: ''
+	};
 
-  state = {
-    authorName: '',
-  }
+	handleOnChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
 
-  handleOnChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
+	handleOnSubmit = (event) => {
+		console.log(this.state);
 
+		event.preventDefault();
+		const author = { ...this.state, id: uuid() };
+		this.props.addAuthor(author);
+		this.setState({
+			authorName: ''
+		});
+	};
 
-  handleOnSubmit = event => {
-    event.preventDefault();
-    const author = {...this.state, id: uuid() };
-    this.props.addAuthor(author);
-    this.setState({
-      authorName: ''
-    });
-  }
+	render() {
+		return (
+			<form onSubmit={(event) => this.handleOnSubmit(event)}>
+				<p>
+					<input
+						type="text"
+						onChange={(event) => this.handleOnChange(event)}
+						name="authorName"
+						value={this.state.authorName}
+						placeholder="author name"
+					/>
+				</p>
+				<input type="submit" />
+			</form>
+		);
+	}
+}
 
-  render() {
-    return(
-      <form onSubmit={(event) => this.handleOnSubmit(event)}>
-        <p>
-          <input
-            type="text"
-            onChange={(event) => this.handleOnChange(event)}
-            name="authorName"
-            value={this.state.authorName}
-            placeholder="author name" />
-        </p>
-        <input type="submit" />
-      </form>
-    );
-  }
+const mapDispatchToProps = (dispatch) => {
+	return { addAuthor: (newAuthor) => dispatch(addAuthor(newAuthor)) };
 };
 
-export default connect(null, { addAuthor })(AuthorInput);
+export default connect(null, mapDispatchToProps)(AuthorInput);
--- src/components/Authors.js
+++ src/components/Authors.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Authors extends Component {
+	render() {
+		console.log(this.props.authors);
 
-  render() {
+		let authors = this.props.authors.map((author) => (
+			<li key={author.id}>{author.authorName}</li>
+		));
 
-    let authors = this.props.authors.map(author => <li key={author.id}>{author.authorName}</li>);
-
-    return (
-      <div>
-        <ul>
-          {authors}
-        </ul>
-      </div>
-    );
-  }
-};
-
-const mapStateToProps = state => {
-  return { authors: state.authors }
+		return (
+			<div>
+				<ul>{authors}</ul>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = (state) => {
+	return { authors: state.authors };
+};
 
 export default connect(mapStateToProps)(Authors);
